17bac96ae1a43c57b1a4ab0d635933abb66984b0
Tatu <pepito2004pepito@gmail.com>
2023-06-05 01:05:27 -0300

    Database hours changes

--- Todo/schema.py
+++ Todo/schema.py
@@ -1,12 +1,6 @@
 instructions = [
-    'SET FOREIGN_KEY_CHECKS=0;',
-    'DROP TABLE IF EXISTS profesores;',
-    'DROP TABLE IF EXISTS user;',
-    'DROP TABLE IF EXISTS talleres;',
-    'DROP TABLE IF EXISTS alumnos;',         
-    'SET FOREIGN_KEY_CHECKS=1;',
 """
-  CREATE TABLE user (
+  CREATE TABLE IF NOT EXISTS user (
       id INT AUTO_INCREMENT PRIMARY KEY,
       username VARCHAR(255) NOT NULL,
       password VARCHAR(255) NOT NULL,
@@ -14,13 +8,13 @@ instructions = [
 );
 """,
 """
-  CREATE TABLE talleres (
+  CREATE TABLE IF NOT EXISTS talleres (
       id INT AUTO_INCREMENT PRIMARY KEY,
       nombre VARCHAR(255) NOT NULL
   );
 """,
 """
-  CREATE TABLE profesores (
+  CREATE TABLE IF NOT EXISTS profesores (
       id INT AUTO_INCREMENT PRIMARY KEY,
       user_id INT,
       taller_id INT,
@@ -29,7 +23,7 @@ instructions = [
 );
 """,
 """
-  CREATE TABLE alumnos (
+  CREATE TABLE IF NOT EXISTS alumnos (
       id INT AUTO_INCREMENT PRIMARY KEY,
       taller_id INT,
       nombre VARCHAR(255) NOT NULL,
